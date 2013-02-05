fbe64c8460a5ef63556e962366a1d35750ce12e4
Niklas Klügel <lodsb@lodsb.org>
2013-02-05 19:23:56 +0100

    moving over to sbt

--- src/org/lodsb/reakt/util/time/sequence/timerclock/nativelinux/linuxclock.h
+++
@@ -1,9 +0,0 @@
-void set_ticktime_nanos(int nanoseconds);
-void next_tick();
-
-int scheduleRealtime(int prio);
-int scheduleFIFO(int prio);
-int scheduleOther(int prio);
-
-int getScheduler(void);
-int getPriority(void);
